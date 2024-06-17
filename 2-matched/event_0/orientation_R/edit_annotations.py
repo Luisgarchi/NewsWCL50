3724e50f5998f50be2a482b5e6550b33a3425193
luisgarchi <luisgarchi@live.com>
2024-06-17 19:36:26 +0200

    Include edit anotation files for completeness

---
+++ 2-matched/event_0/orientation_R/edit_annotations.py
@@ -0,0 +1,41 @@
+import pandas as pd
+"""
+def edit_annotations_file(file_path):
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+    
+    # Delete every entry that has a sentence value of 16
+    df = df[df['sentence'] != 16]
+    
+    # Subtract one from every sentence attribute that is greater than 16
+    df.loc[df['sentence'] > 16, 'sentence'] -= 1
+    
+    # Save the modified DataFrame back to the CSV file
+    df.to_csv(file_path, index=False)
+
+# Specify the path to the CSV file
+annotations_file_path = '0_R_annotations.csv'
+edit_annotations_file(annotations_file_path)
+
+print(f'Edited file: {annotations_file_path}')
+"""
+
+
+def edit_annotations_file(file_path):
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+    
+    # Delete every entry that has a sentence value of 16
+    df = df[df['sentence'] != 22]
+    
+    # Subtract one from every sentence attribute that is greater than 16
+    df.loc[df['sentence'] >= 23, 'sentence'] -= 1
+    
+    # Save the modified DataFrame back to the CSV file
+    df.to_csv(file_path, index=False)
+
+# Specify the path to the CSV file
+annotations_file_path = '0_R_annotations.csv'
+edit_annotations_file(annotations_file_path)
+
+print(f'Edited file: {annotations_file_path}')
---
+++ 2-matched/event_6/orientation_M/edit_annotations.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+def edit_annotations_file(file_path):
+    # Load the CSV file
+    df = pd.read_csv(file_path)
+    
+    # Delete every entry that has a sentence value of 16
+    #df = df[df['sentence'] != 5]
+    
+    # Subtract one from every sentence attribute that is greater than 16
+    df.loc[df['paragraph'] >= 6, 'sentence'] += 1
+    
+    # Save the modified DataFrame back to the CSV file
+    df.to_csv(file_path, index=False)
+
+# Specify the path to the CSV file
+annotations_file_path = '6_M_annotations.csv'
+edit_annotations_file(annotations_file_path)
+
+print(f'Edited file: {annotations_file_path}')
